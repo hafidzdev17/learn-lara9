3d44661aa579a1933a5058dc4c5056b18ba4533f
hafet17 <do.crazy192@gmail.com>
2022-08-02 19:12:35 +0700

    inital commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="min-h-screen bg-red-500 font-sans antialiased">
+        Lorem ipsum dolor sit amets consectetur adipisicing elits.
+    </div>
+</body>
+</html>
